6600050de33c3acb17556a17fbbe94b527c33828
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-11-29 13:34:47 -0800

    dgdf

--- game.py
+++ game.py
@@ -31,7 +31,6 @@ Use of a variables, including a list type variable must be part of your program.
 
 #setup: code on the right side of computer and game on the left side. 
 
-planetCoords=[(350,450),(359,453),(362,456),(365,465),(362,474),(359,477),(350,480),(341,477),(338,474)(335,465),(338,456),(341,453)]
 # (341,477) (338,474) (338,456) (341,453)
 def setup():
     print('Here is the setup. You must have the program running\non the right side and the game running on the left side.')
@@ -41,8 +40,16 @@ def setup():
             print('Thank you, we will now start the game.\nif at any time you would like to exit,\n just type "e"')
             break
 
-def MainTask():
-    pyautogui.mouseDown()
-    for i in planetCoords:
-        pyautogui.moveTo(i[0],i[1],0.5)
+def MainTask():#350,465
+    x=350
+    y=465
+    m=0
+    planetCoords=[[x,y-15],[x+9,y-12],[x+12,y-9],[x+15,y],[x+12,y+9],[x+9,y+12],[x,y+15],[x-9,y+12],[x-12,y+9],[x-15,y],[x-12,y-9],[x-9,y-12]]
+    while m!=10:
+        for i in planetCoords:
+            pyautogui.mouseDown()
+            pyautogui.moveTo(i[0],i[1],0.5)
+        m=m+1
+        
+setup()
 MainTask()
